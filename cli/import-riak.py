635007cbad5a5b1b5316da9a349d49a3841e22b8
chtotonetak <madlabman@mail.ru>
2019-04-26 20:37:43 +0200

    Update Riak import script

--- cli/import-riak.py
+++ cli/import-riak.py
@@ -2,16 +2,12 @@ import sys
 import csv
 import progressbar
 import riak
+import argparse
 
-try:  # checking import file path argument existing
-    filename = sys.argv[1]
-    if len(filename) <= 0:
-        raise Exception
-    print('Importing {}'.format(filename))
-except Exception as error:
-    print('You have to provide filename for import')
-    print(error)
-    sys.exit(-1)
+# defining arguments
+parser = argparse.ArgumentParser(description='Import Feedback from CSV to Riak')
+parser.add_argument('-f', nargs=1, required=True, help='path to the CSV file')
+args = parser.parse_args()
 
 try:
     client = riak.RiakClient(host='kv', pb_port=8087)  # connection to the riak
@@ -20,16 +16,17 @@ except Exception as error:
     sys.exit(-1)
 
 try:
-    with open(filename, 'r') as csvfile:
+    with open(args.f[0], 'r') as csvfile:
         num_lines = sum(1 for line in csvfile); csvfile.seek(0, 0)  # calculate rows count
         filereader = csv.reader(csvfile, delimiter='|', quotechar='\'')
         bar = progressbar.ProgressBar(max_value=num_lines)
         loaded_count = 0
         for row in filereader:
-            bucketname, person_id, feedback = row  # getting values from line
-            bucket = client.bucket(bucketname)  # creating new bucket with asin name
-            bucket.new(person_id, data=feedback).store()  # store feedback by id
+            bucket_name, person_id, feedback = row  # getting values from line
+            bucket = client.bucket(bucket_name)  # creating new bucket with asin name
+            bucket.new(person_id, data=feedback).store()  # store feedback by composite key
             loaded_count += 1; bar.update(loaded_count)  # show progress
+        bar.update()
         print('Data was loaded')
         sys.exit(0)
 except Exception as error:
